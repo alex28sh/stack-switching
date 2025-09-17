ef54abe321dc2902f51f4424f7dd54383a0f4464
Veronika Sirotkina <veronika.sirotkina@jetbrains.com>
2025-09-17 15:40:48 +0200

    sww_util stub

--- interpreter/exec/eval.ml
+++ interpreter/exec/eval.ml
@@ -1281,9 +1281,7 @@ let rec step (c : config) : config =
         vs', [Frame (n2, frame', ([], instr')) @@ e.at]
 
       | Func.HostFunc (_, f) ->
-        let prom = Lib.Promise.make () in
-        Lib.Promise.fulfill prom c.frame.inst;
-        (try List.rev (f prom (List.rev args)) @ vs', []
+        (try List.rev (f (List.rev args)) @ vs', []
         with Crash (_, msg) -> Crash.error e.at msg)
       )
 
